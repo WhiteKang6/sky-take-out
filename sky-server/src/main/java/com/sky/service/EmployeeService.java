b25df28a74f9b8ca8007c6f3791fdf2cce700df0
zhiyin <2385982919@qq.com>
2026-05-09 09:27:23 +0800

    连接alioss+上传图片+新增菜品业务代码开发+修改密钥

---
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -0,0 +1,46 @@
+package com.sky.service;
+
+import com.sky.dto.EmployeeDTO;
+import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
+import com.sky.entity.Employee;
+import com.sky.result.PageResult;
+
+public interface EmployeeService {
+
+    /**
+     * 员工登录
+     * @param employeeLoginDTO
+     * @return
+     */
+    Employee login(EmployeeLoginDTO employeeLoginDTO);
+
+    /**
+     * 新增员工
+     * @param employeeDTO
+     */
+    void save(EmployeeDTO employeeDTO);
+
+    /**
+     * 员工分页查询
+     * @param employeePageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    /**
+     * 启用或者禁用员工
+     * @param status
+     * @param id
+     */
+    void startOrStop(int status, Long id);
+
+    /**
+     * 员工查询
+     * @param id
+     * @return
+     */
+    Employee getById(Long id);
+
+    void update(EmployeeDTO employeeDTO);
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,45 @@
+package com.sky.service.impl;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DishServiceImpl implements DishService {
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
+    /**
+     * 新增菜品
+     * @param dishDTO
+     */
+    @Override
+    public void saveWithFlavor(DishDTO dishDTO) {
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO,dish);
+        //插入1条数据
+        dishMapper.insert(dish);
+        //获取菜品id
+        Long id=dish.getId();
+
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if (flavors !=null && flavors.size()>0) {
+            flavors.forEach(flavor->{
+                flavor.setDishId(id);
+            });
+            //插入菜品口味n条数据
+            dishFlavorMapper.insertBatch(flavors);
+        }
+
+
+    }
+}
